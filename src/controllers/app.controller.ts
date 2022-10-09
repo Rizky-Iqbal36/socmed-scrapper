df576bd8dcb31237cbd984ad1469209c0ca9418b
Rizky-Iqbal36 <rizkiiqbal36@gmail.com>
2022-10-10 02:16:04 +0700

    feat: 🎸 add health check

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common'
-import { AppService } from '../services/app.service'
+import appConfig from '@app/config'
+import moment from 'moment'
+import 'moment-duration-format'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello()
+  @Get('/health-check')
+  public async healthCheck() {
+    return {
+      name: appConfig.app.name,
+      version: appConfig.app.version,
+      environment: appConfig.app.env,
+      uptime: moment.duration(process.uptime(), 'seconds').format('h [hrs] : m [min] : s [sec]', { trim: false })
+    }
   }
 }
